62ee2554bb0d6fa2dfbef317f62ccb6e842393dd
BenIsWassily <34525955+BenIsWassily@users.noreply.github.com>
2022-05-23 22:59:05 +0200

    Fix Issues with Side Traps and Database

--- scripts/jb2a_sequencer.js
+++ scripts/jb2a_sequencer.js
@@ -284,16 +284,16 @@ export async function jb2aFreeDatabase(prefix) {
                 orange: `${prefix}/JB2A_DnD5e/Library/Generic/Fire/Campfire/Campfire_01_Regular_Orange_200x200.webm`,
             },
         }
-        patreonDatabase.celestial_bodies = {
+        freeDatabase.celestial_bodies = {
             planet: {
                 atmo: {
                     '01': {
-                        blueorange: `${prefix}/JB2A_DnD5e/Library/Generic/Celetial_Bodies/PlanetAtmo_01_Regular_BlueOrange_800x800.webm`
+                        blueorange: `${prefix}/JB2A_DnD5e/Library/Generic/Celestial_Bodies/PlanetAtmo_01_Regular_BlueOrange_800x800.webm`
                     },
                 },
                 no_atmo: {
                     '03': {
-                        grey: `${prefix}/JB2A_DnD5e/Library/Generic/Celetial_Bodies/PlanetAtmo_02_Regular_Grey_800x800.webm`
+                        grey: `${prefix}/JB2A_DnD5e/Library/Generic/Celestial_Bodies/PlanetNoAtmo_03_Regular_Grey_800x800.webm`
                     }
                 }
             }
@@ -2294,7 +2294,7 @@ export async function jb2aFreeDatabase(prefix) {
                 purple: `${prefix}/JB2A_DnD5e/Library/Generic/Item/SphereOfAnnihilation_01_Regular_Purple_600x600.webm`
             }
         }
-        patreonDatabase.spike_trap = {
+        freeDatabase.spike_trap = {
             '05x05ft': {
                 top: {
                     holes: {
@@ -2312,7 +2312,7 @@ export async function jb2aFreeDatabase(prefix) {
                         },
                         still_frame: {
                             deployed: `${prefix}/JB2A_DnD5e/Library/Generic/Traps/Spike_Trap/Stills/SpikeTrapTopHolesDeployed01_Regular_Grey_05x05ft_400x400_StillFrame.webp`,
-                            hidden: `${prefix}/JB2A_DnD5e/Library/Generic/Traps/Spike_Trap/Stills/SpikeTrapTopHolesHidden01_Regular_Grey_05x05ft_400x400_StillFrame.webp`
+                            hidden: `${prefix}/JB2A_DnD5e/Library/Generic/Traps/Spike_Trap/Stills/SpikeTrapTopHolesHidden01_Dark_Black_05x05ft_400x400_StillFrame.webp`
                         }
                     }
                 },
@@ -2320,19 +2320,19 @@ export async function jb2aFreeDatabase(prefix) {
                     holes: {
                         normal: {
                             '01': {
-                                '01': `${prefix}/JB2A_DnD5e/Library/Generic/Traps/Spike_Trap/SpikeTrapSideHoles01_01_Regular_Grey_05x05ft_400x400.webm`,
-                                '02': `${prefix}/JB2A_DnD5e/Library/Generic/Traps/Spike_Trap/SpikeTrapSideHoles01_02_Regular_Grey_05x05ft_400x400.webm`
+                                '01': `${prefix}/JB2A_DnD5e/Library/Generic/Traps/Spike_Trap/SpikeTrapSideHoles01_01_Regular_Grey_05x05ft_400x300.webm`,
+                                '02': `${prefix}/JB2A_DnD5e/Library/Generic/Traps/Spike_Trap/SpikeTrapSideHoles01_02_Regular_Grey_05x05ft_400x300.webm`
                             }
                         },
                         rearming: {
                             '01': {
-                                '01': `${prefix}/JB2A_DnD5e/Library/Generic/Traps/Spike_Trap/SpikeTrapSideHolesRearm01_01_Regular_Grey_05x05ft_400x400.webm`,
-                                '02': `${prefix}/JB2A_DnD5e/Library/Generic/Traps/Spike_Trap/SpikeTrapSideHolesRearm01_02_Regular_Grey_05x05ft_400x400.webm`
+                                '01': `${prefix}/JB2A_DnD5e/Library/Generic/Traps/Spike_Trap/SpikeTrapSideHolesRearm01_01_Regular_Grey_05x05ft_400x300.webm`,
+                                '02': `${prefix}/JB2A_DnD5e/Library/Generic/Traps/Spike_Trap/SpikeTrapSideHolesRearm01_02_Regular_Grey_05x05ft_400x300.webm`
                             }
                         },
                         still_frame: {
-                            deployed: `${prefix}/JB2A_DnD5e/Library/Generic/Traps/Spike_Trap/Stills/SpikeTrapSideHolesDeployed01_Regular_Grey_05x05ft_400x400_StillFrame.webp`,
-                            hidden: `${prefix}/JB2A_DnD5e/Library/Generic/Traps/Spike_Trap/Stills/SpikeTrapSideHolesHidden01_Regular_Grey_05x05ft_400x400_StillFrame.webp`
+                            deployed: `${prefix}/JB2A_DnD5e/Library/Generic/Traps/Spike_Trap/Stills/SpikeTrapSideHolesDeployed01_Regular_Grey_05x05ft_400x300_StillFrame.webp`,
+                            hidden: `${prefix}/JB2A_DnD5e/Library/Generic/Traps/Spike_Trap/Stills/SpikeTrapSideHolesHidden01_Dark_Black_05x05ft_400x300_StillFrame.webp`
                         }
                     }
                 }
@@ -2354,7 +2354,7 @@ export async function jb2aFreeDatabase(prefix) {
                         },
                         still_frame: {
                             deployed: `${prefix}/JB2A_DnD5e/Library/Generic/Traps/Spike_Trap/Stills/SpikeTrapTopHolesDeployed01_Regular_Grey_10x05ft_600x400_StillFrame.webp`,
-                            hidden: `${prefix}/JB2A_DnD5e/Library/Generic/Traps/Spike_Trap/Stills/SpikeTrapTopHolesHidden01_Regular_Grey_10x05ft_600x400_StillFrame.webp`
+                            hidden: `${prefix}/JB2A_DnD5e/Library/Generic/Traps/Spike_Trap/Stills/SpikeTrapTopHolesHidden01_Dark_Black_10x05ft_600x400_StillFrame.webp`
                         }
                     }
                 },
@@ -2362,19 +2362,19 @@ export async function jb2aFreeDatabase(prefix) {
                     holes: {
                         normal: {
                             '01': {
-                                '01': `${prefix}/JB2A_DnD5e/Library/Generic/Traps/Spike_Trap/SpikeTrapSideHoles01_01_Regular_Grey_10x05ft_600x400.webm`,
-                                '02': `${prefix}/JB2A_DnD5e/Library/Generic/Traps/Spike_Trap/SpikeTrapSideHoles01_02_Regular_Grey_10x05ft_600x400.webm`
+                                '01': `${prefix}/JB2A_DnD5e/Library/Generic/Traps/Spike_Trap/SpikeTrapSideHoles01_01_Regular_Grey_10x05ft_600x300.webm`,
+                                '02': `${prefix}/JB2A_DnD5e/Library/Generic/Traps/Spike_Trap/SpikeTrapSideHoles01_02_Regular_Grey_10x05ft_600x300.webm`
                             },
                         },
                         rearming: {
                             '01': {
-                                '01': `${prefix}/JB2A_DnD5e/Library/Generic/Traps/Spike_Trap/SpikeTrapSideHolesRearm01_01_Regular_Grey_10x05ft_600x400.webm`,
-                                '02': `${prefix}/JB2A_DnD5e/Library/Generic/Traps/Spike_Trap/SpikeTrapSideHolesRearm01_02_Regular_Grey_10x05ft_600x400.webm`
+                                '01': `${prefix}/JB2A_DnD5e/Library/Generic/Traps/Spike_Trap/SpikeTrapSideHolesRearm01_01_Regular_Grey_10x05ft_600x300.webm`,
+                                '02': `${prefix}/JB2A_DnD5e/Library/Generic/Traps/Spike_Trap/SpikeTrapSideHolesRearm01_02_Regular_Grey_10x05ft_600x300.webm`
                             },
                         },
                         still_frame: {
-                            deployed: `${prefix}/JB2A_DnD5e/Library/Generic/Traps/Spike_Trap/Stills/SpikeTrapSideHolesDeployed01_Regular_Grey_10x05ft_600x400_StillFrame.webp`,
-                            hidden: `${prefix}/JB2A_DnD5e/Library/Generic/Traps/Spike_Trap/Stills/SpikeTrapSideHolesHidden01_Regular_Grey_10x05ft_600x400_StillFrame.webp`
+                            deployed: `${prefix}/JB2A_DnD5e/Library/Generic/Traps/Spike_Trap/Stills/SpikeTrapSideHolesDeployed01_Regular_Grey_10x05ft_600x300_StillFrame.webp`,
+                            hidden: `${prefix}/JB2A_DnD5e/Library/Generic/Traps/Spike_Trap/Stills/SpikeTrapSideHolesHidden01_Dark_Black_10x05ft_600x300_StillFrame.webp`
                         }
                     }
                 }
@@ -2396,7 +2396,7 @@ export async function jb2aFreeDatabase(prefix) {
                         },
                         still_frame: {
                             deployed: `${prefix}/JB2A_DnD5e/Library/Generic/Traps/Spike_Trap/Stills/SpikeTrapTopHolesDeployed01_Regular_Grey_10x10ft_600x600_StillFrame.webp`,
-                            hidden: `${prefix}/JB2A_DnD5e/Library/Generic/Traps/Spike_Trap/Stills/SpikeTrapTopHolesHidden01_Regular_Grey_10x10ft_600x600_StillFrame.webp`
+                            hidden: `${prefix}/JB2A_DnD5e/Library/Generic/Traps/Spike_Trap/Stills/SpikeTrapTopHolesHidden01_Dark_Black_10x10ft_600x600_StillFrame.webp`
                         }
                     }
                 },
@@ -2404,19 +2404,19 @@ export async function jb2aFreeDatabase(prefix) {
                     holes: {
                         normal: {
                             '01': {
-                                '01': `${prefix}/JB2A_DnD5e/Library/Generic/Traps/Spike_Trap/SpikeTrapSideHoles01_01_Regular_Grey_10x10ft_600x400.webm`,
-                                '02': `${prefix}/JB2A_DnD5e/Library/Generic/Traps/Spike_Trap/SpikeTrapSideHoles01_02_Regular_Grey_10x10ft_600x400.webm`
+                                '01': `${prefix}/JB2A_DnD5e/Library/Generic/Traps/Spike_Trap/SpikeTrapSideHoles01_01_Regular_Grey_10x10ft_600x300.webm`,
+                                '02': `${prefix}/JB2A_DnD5e/Library/Generic/Traps/Spike_Trap/SpikeTrapSideHoles01_02_Regular_Grey_10x10ft_600x300.webm`
                             }
                         },
                         rearming: {
                             '01': {
-                                '01': `${prefix}/JB2A_DnD5e/Library/Generic/Traps/Spike_Trap/SpikeTrapSideHolesRearm01_01_Regular_Grey_10x10ft_600x400.webm`,
-                                '02': `${prefix}/JB2A_DnD5e/Library/Generic/Traps/Spike_Trap/SpikeTrapSideHolesRearm01_02_Regular_Grey_10x10ft_600x400.webm`
+                                '01': `${prefix}/JB2A_DnD5e/Library/Generic/Traps/Spike_Trap/SpikeTrapSideHolesRearm01_01_Regular_Grey_10x10ft_600x300.webm`,
+                                '02': `${prefix}/JB2A_DnD5e/Library/Generic/Traps/Spike_Trap/SpikeTrapSideHolesRearm01_02_Regular_Grey_10x10ft_600x300.webm`
                             }
                         },
                         still_frame: {
-                            deployed: `${prefix}/JB2A_DnD5e/Library/Generic/Traps/Spike_Trap/Stills/SpikeTrapSideHolesDeployed01_Regular_Grey_10x10ft_600x400_StillFrame.webp`,
-                            hidden: `${prefix}/JB2A_DnD5e/Library/Generic/Traps/Spike_Trap/Stills/SpikeTrapSideHolesHidden01_Regular_Grey_10x10ft_600x400_StillFrame.webp`
+                            deployed: `${prefix}/JB2A_DnD5e/Library/Generic/Traps/Spike_Trap/Stills/SpikeTrapSideHolesDeployed01_Regular_Grey_10x10ft_600x300_StillFrame.webp`,
+                            hidden: `${prefix}/JB2A_DnD5e/Library/Generic/Traps/Spike_Trap/Stills/SpikeTrapSideHolesHidden01_Dark_Black_10x10ft_600x300_StillFrame.webp`
                         }
                     }
                 }
